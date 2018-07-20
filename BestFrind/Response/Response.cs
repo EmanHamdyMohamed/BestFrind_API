f41d090421274125ba4742e8e4d0920e38164d55
EmanHamdyMohamed <emanhamdy645@gmail.com>
2018-07-20 21:20:43 +0200

    Add files via upload

---
+++ BestFrind/Response/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestFrind.Response
+{
+    public class Response<T>
+    {
+        public T Result { get; set; }
+
+        public int StatusCode { get; set; }
+        public string message { get; set; }
+    }
+}
